570781a10ddeba260f2873ea2bef340ff6141276
ludike23 <ludike23@gmail.com>
2022-07-23 10:13:13 +0100

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,34 @@
+<div class="uk-container">
+	<nav class="uk-navbar-container uk-margin" uk-navbar>
+
+		<div class="nav-overlay uk-navbar-left">
+	
+			<a class="uk-navbar-item uk-logo" href="#">Logo</a>
+	
+			<ul class="uk-navbar-nav">
+				<li class="uk-active"><a href="#">Active</a></li>
+				<li><a href="#">Item</a></li>
+			</ul>
+	
+		</div>
+	
+		<div class="nav-overlay uk-navbar-right">
+	
+			<a class="uk-navbar-toggle" uk-search-icon uk-toggle="target: .nav-overlay; animation: uk-animation-fade" href="#"></a>
+	
+		</div>
+	
+		<div class="nav-overlay uk-navbar-left uk-flex-1" hidden>
+	
+			<div class="uk-navbar-item uk-width-expand">
+				<form class="uk-search uk-search-navbar uk-width-1-1">
+					<input class="uk-search-input" type="search" placeholder="Search" autofocus>
+				</form>
+			</div>
+	
+			<a class="uk-navbar-toggle" uk-close uk-toggle="target: .nav-overlay; animation: uk-animation-fade" href="#"></a>
+	
+		</div>
+	
+	</nav>
+</div>
